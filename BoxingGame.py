e5aa05d9fa4d4099ec2043c2cba4dd54d62dba76
ARamos901 <136763187+ARamos901@users.noreply.github.com>
2024-04-17 01:42:34 -0400

    Update BoxingGame.py
    Fixed the  user input for first Boxer with peers help. Need to fix CPU Boxer.

--- BoxingGame.py
+++ BoxingGame.py
@@ -14,10 +14,23 @@ class Boxer:
         self.dmg = dmg
         self.quick = quick
         Boxer.playerlist.append(self.name)
-        
-    def __str__(self):
-        return Boxer.playerlist
-    
+
+    @staticmethod
+    def search(name):
+      for i in range(len(Boxer.playerlist)-1):
+        if Boxer.playerlist[i].lower()== name.lower():
+          return True
+      return False
+
+    '''@staticmethod
+    def show():
+        string='['
+        for element in Boxer.playerlist:
+          string+=element
+          string+=','
+        string+=']'
+        print(string)'''
+
     def attack(self, opponent):
         damage = random.randint(0, self.dmg)
         dodge_chance = random.randint(0, self.quick)
@@ -32,31 +45,30 @@ frank = Boxer("Frank Rodgers", 200, 70, 40)
 jay = Boxer("Jay Neil", 170, 40, 70)
 issac = Boxer("Issac Xaiver", 200, 55, 55)
 
-
+'''print(Boxer.playerlist)
+Boxer.show()'''
 
 while True:
-    user_boxer = input("Please select the Boxer you would like to play as: ").lower()
-    
-    if isinstance(user_boxer,Boxer):
+    user_boxer = input("Please select the Boxer you would like to play as: ")
+#it works if you input "ray stevens"
+    if Boxer.search(user_boxer)==True:
         print(f"Okay, you have chosen {user_boxer} as your boxer.")
-        break  # Exit the loop since a valid boxer is chosen
-    if user_boxer=="q":
-        break
+        next # Exit the loop since a valid boxer is chosen
     else:
         print("Sorry, that is not a Boxer. Please choose again.")
         continue
 
-    
-    cpu_boxer = input("Please select the Boxer you would like to face: ")  #.lower()
-    
+
+    cpu_boxer = input("Please select the Boxer you would like to face: ")
+
     if cpu_boxer=="q":
         break
-    
-    cpu_boxer = next((boxer for boxer in Boxers if boxer.name == cpu_boxer_name), None)
+
+    cpu_boxer = next((boxer for boxer in playerlist if boxer.name == cpu_boxer_name), None)
     if not cpu_boxer:
         print("Sorry, that is not a Boxer. Please choose again.")
         continue
-    
+
     if cpu_boxer == user_boxer:
         print("You cannot face the same boxer you are playing as. Please choose another boxer.")
     else:
@@ -65,6 +77,8 @@ while True:
         print(f"You selected {cpu_boxer.name} as your opponent.")
         break  # Exit the loop since both boxers are selected
 
+    round(user_boxer, cpu_boxer)
+
 def round(boxer1, boxer2):
     print(f"{boxer1.name} vs {boxer2.name}")
     attacker = random.choice([boxer1, boxer2])
@@ -72,10 +86,6 @@ def round(boxer1, boxer2):
 
     attacker.attack(defender)
     print(f"{defender.name} has {defender.hp} health remaining.\n")
-
-round(user_boxer, cpu_boxer)
-
-round()
 
 
 # In[ ]:
